e96d46bfc1b323a55decd4be009817a97445af96
Craig Crosby <creecros@gmail.com>
2018-10-26 10:57:05 -0400

    Update top.php
    Restrict to admins

--- Template/layout/top.php
+++ Template/layout/top.php
@@ -1,4 +1,6 @@
+<?php if ($_SESSION['user']['role'] == 'app-admin'): ?>
 <div class="top-bar">
     <b><?= $this->Notifier->renderUpdatekanboard() ?></b>
     <?= $this->Notifier->renderUpdatePlugin() ?>
 </div>
+<?php endif ?>
