31361ab30e01f728e61e51fba569ce4ba83ca9c9
Craig Crosby <creecros@gmail.com>
2019-01-29 11:47:26 -0500

    Update sidebar.php

--- Template/plugin/sidebar.php
+++ Template/plugin/sidebar.php
@@ -1,1 +1,14 @@
-HOOK TO SIDEBAR FOR LATEST PLUGINS WILL GO TO CONTROLLER FIRST
+<!-- ADD <li> TO SIDEBAR FOR LATEST PLUGINS WILL GO TO CONTROLLER FIRST -->
+<div class="sidebar">
+    <ul>
+        <li <?= $this->app->checkMenuSelection('PluginController', 'show') ?>>
+            <?= $this->url->link(t('Installed Plugins'), 'PluginController', 'show') ?>
+        </li>
+        <li <?= $this->app->checkMenuSelection('PluginController', 'directory') ?>>
+            <?= $this->url->link(t('Plugin Directory'), 'PluginController', 'directory') ?>
+        </li>
+        <li <?= $this->app->checkMenuSelection('PluginTimestampedController', 'show', ['plugin' => 'updatenotifiter']) ?>>
+            <?= $this->url->link(t('Latest Plugins'), 'PluginTimestampedController', 'show', ['plugin' => 'updatenotifiter']) ?>
+        </li>
+    </ul>
+</div>
