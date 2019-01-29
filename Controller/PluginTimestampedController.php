54e1b297e903c8ce21598da9bc02fc9675a55e07
Craig Crosby <creecros@gmail.com>
2019-01-29 11:40:02 -0500

    Create PluginTimestampedController.php

---
+++ Controller/PluginTimestampedController.php
@@ -0,0 +1,18 @@
+
+<?php
+
+namespace Kanboard\Plugin\UpdateNotifier\Controller;
+
+use Kanboard\Controller\BaseController;
+
+class PluginTimestampedController extends BaseController
+{
+    public function show()
+    {
+    }
+
+    public function readJsonAndAddToDB()
+    {
+    }
+
+}
