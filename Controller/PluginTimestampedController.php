a3ab30000bf0c1b1f0909dfe80bb50c414d20c95
Craig Crosby <creecros@gmail.com>
2019-01-29 12:18:55 -0500

    Update PluginTimestampedController.php

--- Controller/PluginTimestampedController.php
+++ Controller/PluginTimestampedController.php
@@ -3,16 +3,38 @@
 
 namespace Kanboard\Plugin\UpdateNotifier\Controller;
 
+use Kanboard\Plugin\UpdateNotifier\Model\PluginTimestampedModel;
+use Kanboard\Core\Plugin\Directory;
+use Kanboard\Core\Plugin\Installer;
+use Kanboard\Core\Plugin\PluginInstallerException;
 use Kanboard\Controller\BaseController;
 
 class PluginTimestampedController extends BaseController
 {
     public function show()
     {
-    }
-
-    public function readJsonAndAddToDB()
-    {
+        $availablePlugins = Directory::getInstance($this->container)->getAvailablePlugins();
+        $installedPlugins = array();
+        
+        foreach ($this->pluginLoader->getPlugins() as $plugin) {
+            $installedPlugins[$plugin->getPluginName()] = $plugin->getPluginVersion();
+        }
+        
+        foreach ($availablePlugins as $name => $value) {
+            $this->pluginTimestampedModel->save($name);
+        }
+        
+        $onlyTenLatestPlugins = $this->pluginTimestampedModel->getTen();
+        
+        foreach ($availablePlugins as $name => $value) {
+            if (!in_array($name, $onlyTenLatestPlugins) { unset($availablePlugins[$name]); }
+        }
+        
+        $this->response->html($this->helper->layout->plugin('updatenotifier:plugin/latest_plugins', array(
+            'available_plugins' => $availablePlugins,
+            'title' => t('Plugin Directory'),
+            'is_configured' => Installer::isConfigured(),
+        )));
     }
 
 }
