445fd69b02b6273fd4d883871eaaa72f0e7098c2
Craig Crosby <creecros@gmail.com>
2019-01-29 14:20:37 -0500

    Update PluginTimestampedController.php

--- Controller/PluginTimestampedController.php
+++ Controller/PluginTimestampedController.php
@@ -12,6 +12,8 @@ class PluginTimestampedController extends BaseController
 {
     public function show()
     {
+        $set_plugins = $this->configModel->get('plugins_are_set', 0);
+        
         $availablePlugins = Directory::getInstance($this->container)->getAvailablePlugins();
         $installedPlugins = array();
         
@@ -19,9 +21,17 @@ class PluginTimestampedController extends BaseController
             $installedPlugins[$plugin->getPluginName()] = $plugin->getPluginVersion();
         }
         
-        foreach ($availablePlugins as $name => $value) {
-            $this->pluginTimestampedModel->save($name);
+        if ($set_plugins = 1) {
+            foreach ($availablePlugins as $name => $value) {
+                $this->pluginTimestampedModel->save($name);
+            }
+        } else {
+            foreach ($availablePlugins as $name => $value) {
+                $this->pluginTimestampedModel->saveWithoutTimestamp($name);
+            }
         }
+        
+        $this->configModel->save(array('plugins_are_set' => 1));
         
         $onlyTenLatestPlugins = $this->pluginTimestampedModel->getTen();
         
