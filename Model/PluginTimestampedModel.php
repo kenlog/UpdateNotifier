31bdb9716367aa958e2ae3ecf6b6d8b646613d61
Craig Crosby <creecros@gmail.com>
2019-01-29 11:07:19 -0500

    Create PluginTimestampedModel.php

---
+++ Model/PluginTimestampedModel.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Kanboard\Plugin\UpdateNotifier\Model;
+
+use Kanboard\Core\Base;
+
+class PluginTimestampedModel extends Base
+{
+
+    const TABLE = 'plugin_dates';
+
+    public function getAll()
+    {
+        return $this->db
+            ->table(self::TABLE)
+            ->desc('date_creation')
+            ->findAll();
+    }
+    
+    public function getTen()
+    {
+        return $this->db
+            ->table(self::TABLE)
+            ->desc('date_creation')
+            ->limit(10)
+            ->findAll();
+    }
+
+    public function exists($name)
+    {
+        return $this->db
+            ->table(self::TABLE)
+            ->eq('name', $name)
+            ->exists();
+    }
+
+    public function save($name)
+    {
+        $results = array();
+        $timestamp = time();
+
+        $this->db->startTransaction();
+        
+        if (!$this->exists($name)) {
+                $results[] = $this->db->table(self::TABLE)->insert(array(
+                    'name' => $name,
+                    'date_creation' => $timestamp,
+                ));
+         }   
+
+        $this->db->closeTransaction();
+        return ! in_array(false, $results, true);
+    }
+
+}
