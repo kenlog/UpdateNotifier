ff975796468fbfcaf821df62c5ce95fa9b39a74a
Craig Crosby <creecros@gmail.com>
2019-05-29 13:35:05 -0400

    version # 1.4.2

--- Plugin.php
+++ Plugin.php
@@ -50,7 +50,7 @@ class Plugin extends Base
 
     public function getPluginVersion()
     {
-        return '1.4.1';  
+        return '1.4.2';  
     }
 
     public function getCompatibleVersion()
