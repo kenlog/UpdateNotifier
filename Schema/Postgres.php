1ff4c1d82b68d9565c9672270d395e709a4bcfb0
Craig Crosby <creecros@gmail.com>
2019-07-01 05:34:48 +0000

    fix #13

--- Schema/Postgres.php
+++ Schema/Postgres.php
@@ -4,12 +4,12 @@ namespace Kanboard\Plugin\UpdateNotifier\Schema;
 
 use PDO;
 
-const VERSION = 1;
+const VERSION = 2;
 
-function version_1(PDO $pdo)
+function version_2(PDO $pdo)
 {
     $pdo->exec("
-        CREATE TABLE IF NOT EXISTS plugin_schema_versions (
+        CREATE TABLE IF NOT EXISTS plugin_dates (
             name VARCHAR(80) NOT NULL PRIMARY KEY,
             date_creation INTEGER NOT NULL DEFAULT 0
         )
