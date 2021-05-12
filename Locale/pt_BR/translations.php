a030291f6fa212c732dd31a25691346c5f411676
Luis <luisfelipeandrade@gmail.com>
2021-05-12 10:41:30 -0300

    Add portuguese translation

---
+++ Locale/pt_BR/translations.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'New update:' => 'Nova atualização:',
+    'New version: Kanboard v' => 'Nova verão: Kanboard v',
+    'Filter by name or content' => 'Filtra pelo nome do conteúdo',
+    'Search...' => 'Pesquisar...',
+    'Latest Plugins' => 'Últimas versões',
+    'There are no new plugins available at this moment.' => 'Não há novos plug-ins disponíveis no momento.'
+];
