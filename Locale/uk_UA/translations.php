e516cd3b470223b4e3ede369052068bdfd45f825
sashaoshurkov <sashaoshurkov@gmail.com>
2023-03-04 18:24:15 +0300

    add Ukraine locale

---
+++ Locale/uk_UA/translations.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'New update:' => 'Нове оновлення:',
+    'New version: Kanboard v' => 'Нова версія: Kanboard v',
+    'Filter by name or content' => 'Фільтрувати за назвою або вмістом',
+    'Search...' => 'Пошук...',
+    'Latest Plugins' => 'Оновлення плагінів',
+    'There are no new plugins available at this moment.' => 'На даний момент оновлень для плагінів немає.'
+];
